7b14758d5167bb0fc8212ddffe10409dfbc6a891
huanghui <815189517@qq.com>
2019-10-31 15:01:57 +0800

    'dynamic-passing-value'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
-    render() { 
-        return ( 
-             <h2>huanghui.com</h2>
-         );
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [
+                { articleId: 1, title: 'hh-blog-1' },
+                { articleId: 2, title: 'hh-blog-2' },
+                { articleId: 3, title: 'hh-blog-3' }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2>huanghui.com</h2>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link to={`/list/${item.articleId}`}>{item.title}</Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+
+        )
     }
 }
- 
+
 export default Home;
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
     render() { 
         return (
-            <h2>List-page</h2>
+            <h2>List-page-{this.state.id}</h2>
         );
+    }
+    componentDidMount() {
+        let tempId = this.props.match.params.id
+        this.setState({
+            id:tempId
+        })
     }
 }
  
