79ca8fbea7ea8cd8abf3deb190da9b4e6cb6e93e
huanghui <815189517@qq.com>
2019-10-31 14:22:20 +0800

    'react-router-install'

---
+++ src/AppRouter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Index = () => {
+    return (
+        <h2>huanghui.com</h2>
+    )
+}
+
+const List = () => {
+    return (
+        <h2>List-page</h2>
+    )
+}
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <ul>
+                <li><Link to='/'>首页</Link></li>
+                <li><Link to='/list/'>列表</Link></li>
+            </ul>
+            <Route path='/' exact component={Index}/>
+            <Route path='/list/' component={List}/>
+        </Router>
+    )
+}
+ 
+export default AppRouter;
