22694e0fdb5a623cc814b4cf15359c9dd110e9a4
huanghui <815189517@qq.com>
2019-11-01 11:29:40 +0800

    'dynamic-route'

--- demo02/src/AppRouter.js
+++ demo02/src/AppRouter.js
@@ -6,25 +6,37 @@ import Career from './Pages/Career'
 import './css/index.css'
 
 const AppRouter = () => {
+    let routeConfig = [
+        { path: '/', title: '博客首页', exact: true, component: Index },
+        { path: '/video/', title: '视频教程', exact: false, component: Video },
+        { path: '/career/', title: '职场技能', exact: false, component: Career }
+    ]
     return (
         <Router>
             <div className='mainDiv'>
                 <div className='leftNav'>
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to='/'>博客首页</Link></li>
-                        <li><Link to='/video/'>视频教程</Link></li>
-                        <li><Link to='/career/'>职场技能</Link></li>
+                        {
+                            routeConfig.map((item,index) => {
+                                return (
+                                    <li key={index}><Link to={item.path}>{item.title}</Link></li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
                 <div className='rightMain'>
-                    <Route path='/' exact component={Index} />
-                    <Route path='/video/' component={Video} />
-                    <Route path='/career/' component={Career} />
+                    {
+                        routeConfig.map((item, index) => {
+                            return (
+                                <Route key={index} path={item.path} exact={item.exact} component={item.component} />
+                            )
+                        })
+                    }
                 </div>
             </div>
         </Router>
     )
-
 }
 export default AppRouter
